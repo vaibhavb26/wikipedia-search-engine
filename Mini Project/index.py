155f791d50fb5a701f140b942025b3990fa58b47
vaibhav bajaj <vaibhav.bajaj@research.iiit.ac.in>
2019-09-12 00:01:36 +0530

    search update

--- Mini Project/index.py
+++ Mini Project/index.py
@@ -2,13 +2,20 @@ import sys
 from collections import defaultdict
 import re
 import xml.sax
+import heapq
 import nltk
 import json
 from nltk.corpus import stopwords
 import Stemmer
+import timeit
+from tqdm import tqdm
+import threading
 
 
 currPage = 0
+fileCount = 0
+offset = 0
+fileCount = 0
 inverted_index = defaultdict(list)
 title_dic = defaultdict(str)
 
@@ -21,7 +28,10 @@ def Indexing(dic):
 
     global currPage
     global indexMap
-
+    global title_dic
+    global offset
+    global fileCount
+    
     ID = currPage
     arr = ["title", "body", "info", "cat", "links", "ref"]
     pageDic = {}
@@ -36,7 +46,7 @@ def Indexing(dic):
             words[word] += 1
         
     for word in words.keys():
-        string = str(currPage)
+        string = 'd' + str(currPage)
 
         t = pageDic["title"][word]
         if t:
@@ -64,10 +74,258 @@ def Indexing(dic):
         
         inverted_index[word].append(string)    
     currPage += 1
+    if currPage % 20000 == 0:
+        offset = writeIntoFile(inverted_index, title_dic, fileCount, offset)
+        indexMap = defaultdict(list)
+        title_dic = {}
+        fileCount += 1
+
+
+def mergeFiles(fileCount):
+
+    words = {}
+    files = {}
+    top = {}
+    flag = [0] * fileCount
+    data = defaultdict(list)
+    heap = []
+    finalCount = 0
+    offsetSize = 0
+
+    for i in range(fileCount):
+        filename = '../data/index' + str(i) + '.txt'
+        files[i] = open(filename, 'r')
+        flag[i] = 1
+        top[i] = files[i].readline().strip()
+        words[i] = top[i].split()
+        print(i)
+        if words[i][0] not in heap:
+            heapq.heappush(heap, words[i][0])
+
+    count = 0
+    while any(flag) == 1:
+        temp = heapq.heappop(heap)
+        count += 1
+        # print(count)
+        if count % 100000 == 0:
+            oldFileCount = finalCount
+            finalCount, offsetSize = writeFinalIndex(data, finalCount, offsetSize)
+            if oldFileCount != finalCount:
+                data = defaultdict(list)
+        for i in range(fileCount):
+            if flag[i]:
+                if words[i][0] == temp:
+                    data[temp].extend(words[i][1:])
+                    top[i] = files[i].readline().strip()
+                    if top[i] == '':
+                        flag[i] = 0
+                        files[i].close()
+                        # os.remove('../data/index' + str(i) + '.txt')
+                    else:
+                        words[i] = top[i].split()
+                        if words[i][0] not in heap:
+                            heapq.heappush(heap, words[i][0])
+                        
+    finalCount, offsetSize = writeFinalIndex(data, finalCount, offsetSize)
+
+def writeFinalIndex(data, finalCount, offsetSize):
+
+    title = defaultdict(dict)
+    body = defaultdict(dict)
+    info = defaultdict(dict)
+    category = defaultdict(dict)
+    link = defaultdict(dict)
+    reference = defaultdict(dict)
+    distinctWords = []
+    offset = []
+
+    for key in tqdm(sorted(data.keys())):
+        docs = data[key]
+        temp = []
+        
+        for i in range(len(docs)):
+            posting = docs[i]
+            docID = re.sub(r'.*d([0-9]*).*', r'\1', posting)
+            
+            temp = re.sub(r'.*t([0-9]*).*', r'\1', posting)
+            if temp != posting:
+                title[key][docID] = float(temp)
+            
+            temp = re.sub(r'.*b([0-9]*).*', r'\1', posting)
+            if temp != posting:
+                body[key][docID] = float(temp)
+
+            temp = re.sub(r'.*i([0-9]*).*', r'\1', posting)
+            if temp != posting:
+                info[key][docID] = float(temp)
+
+            temp = re.sub(r'.*c([0-9]*).*', r'\1', posting)
+            if temp != posting:
+                category[key][docID] = float(temp)
+
+            temp = re.sub(r'.*l([0-9]*).*', r'\1', posting)
+            if temp != posting:
+                link[key][docID] = float(temp)
+            
+            temp = re.sub(r'.*r([0-9]*).*', r'\1', posting)
+            if temp != posting:
+                reference[key][docID] = float(temp)
+
+        string = key + ' ' + str(finalCount) + ' ' + str(len(docs))
+        distinctWords.append(string)
+        offset.append(str(offsetSize))
+        offsetSize += len(string) + 1
+
+    titleData = []
+    titleOffset = []
+    prevTitle = 0
+
+    bodyData = []
+    bodyOffset = []
+    prevBody = 0
+    
+    infoData = []
+    infoOffset = []
+    prevInfo = 0
+    
+    linkData = []
+    linkOffset = []
+    prevLink = 0
+    
+    categoryData = []
+    categoryOffset = []
+    prevCategory = 0
+    
+    referenceOffset = []
+    referenceData = []
+    prevReference = 0
+
+    for key in tqdm(sorted(data.keys())):
+
+        if key in title:
+            string = key + ' '
+            docs = title[key]
+            docs = sorted(docs, key = docs.get, reverse=True)
+            for doc in docs:
+                string += doc + ' ' + str(title[key][doc]) + ' '
+            titleOffset.append(str(prevTitle) + ' ' + str(len(docs)))
+            prevTitle += len(string) + 1
+            titleData.append(string)
+
+        if key in body:
+            string = key + ' '
+            docs = body[key]
+            docs = sorted(docs, key = docs.get, reverse=True)
+            for doc in docs:
+                string += doc + ' ' + str(body[key][doc]) + ' '
+            bodyOffset.append(str(prevBody) + ' ' + str(len(docs)))
+            prevBody += len(string) + 1
+            bodyData.append(string)
+
+        if key in info:
+            string = key + ' '
+            docs = info[key]
+            docs = sorted(docs, key = docs.get, reverse=True)
+            for doc in docs:
+                string += doc + ' ' + str(info[key][doc]) + ' '
+            infoOffset.append(str(prevInfo) + ' ' + str(len(docs)))
+            prevInfo += len(string) + 1
+            infoData.append(string)
+
+        if key in category:
+            string = key + ' '
+            docs = category[key]
+            docs = sorted(docs, key = docs.get, reverse=True)
+            for doc in docs:
+                string += doc + ' ' + str(category[key][doc]) + ' '
+            categoryOffset.append(str(prevCategory) + ' ' + str(len(docs)))
+            prevCategory += len(string) + 1
+            categoryData.append(string)
+
+        if key in link:
+            string = key + ' '
+            docs = link[key]
+            docs = sorted(docs, key = docs.get, reverse=True)
+            for doc in docs:
+                string += doc + ' ' + str(link[key][doc]) + ' '
+            linkOffset.append(str(prevLink) + ' ' + str(len(docs)))
+            prevLink += len(string) + 1
+            linkData.append(string)
+
+        if key in reference:
+            string = key + ' '
+            docs = reference[key]
+            docs = sorted(docs, key = docs.get, reverse=True)
+            for doc in docs:
+                string += doc + ' ' + str(reference[key][doc]) + ' '
+            referenceOffset.append(str(prevReference) + ' ' + str(len(docs)))
+            prevReference += len(string) + 1
+            referenceData.append(string)
+
+    thread = []
+    
+    thread.append(writeThread('t', titleData, titleOffset, finalCount))
+    thread.append(writeThread('b', bodyData, bodyOffset, finalCount))
+    thread.append(writeThread('i', infoData, infoOffset, finalCount))
+    thread.append(writeThread('c', categoryData, categoryOffset, finalCount))
+    thread.append(writeThread('l', linkData, linkOffset, finalCount))
+    thread.append(writeThread('r', referenceData, referenceOffset, finalCount))
+
+    for i in range(6):
+        thread[i].start()
+
+    for i in range(6):
+        thread[i].join()
+
+    with open('../data/vocab.txt', 'a') as f:
+        f.write('\n'.join(distinctWords))
+        f.write('\n')
+
+    with open('../data/offset.txt', 'a') as f:
+        f.write('\n'.join(offset))
+        f.write('\n')
+
+    return finalCount+1, offsetSize
+
+
+def writeIntoFile(index, dictID, fileCount, titleOffset):
+
+    # print("yo")
+    prevTitleOffset = titleOffset
+    data = []
+    for key in sorted(index.keys()):
+        string = key + ' '
+        postings = index[key]
+        string += ' '.join(postings)
+        data.append(string)
+
+    filename = '../data/index' + str(fileCount) + '.txt'
+    with open(filename, 'w') as f:
+        f.write('\n'.join(data))
+
+    data = []
+    dataOffset = []
+    for key in sorted(dictID):
+        temp = str(key) + ' ' + dictID[key].strip()
+        data.append(temp)
+        dataOffset.append(str(prevTitleOffset))
+        prevTitleOffset += len(temp) + 1
+
+    filename = '../data/title.txt'
+    with open(filename, 'a') as f:
+        f.write('\n'.join(data))
+        f.write('\n')
+    
+    filename = '../data/titleOffset.txt'
+    with open(filename, 'a') as f:
+        f.write('\n'.join(dataOffset))
+        f.write('\n')
+
+    return prevTitleOffset
+
 
 
 def stemming(text):
-    # Should not be used. use dict instead.    
     return stemmer.stemWords(text)
 
 def tokenization(data):
@@ -79,8 +337,6 @@ def tokenization(data):
 
 
 def removeStopwords(text):
-    
-    # need to check for faster performance. O(n^2) the following loop.
     return [x for x in text if x not in stop_words]
 
 def processTitle(title):
@@ -188,6 +444,27 @@ def preprocess(title, text):
     return title, info, body, ref, links, cat
 
 
+class writeThread(threading.Thread):
+
+    def __init__(self, field, data, offset, count):
+
+        threading.Thread.__init__(self)
+        self.field = field
+        self.data = data
+        self.count = count
+        self.offset = offset
+
+    def run(self):
+
+        filename = '../data/' + self.field + str(self.count) + '.txt'
+        with open(filename, 'w') as f:
+            f.write('\n'.join(self.data))
+        
+        filename = '../data/offset_' + self.field + str(self.count) + '.txt'
+        with open(filename, 'w') as f:
+            f.write('\n'.join(self.offset))
+
+
 class docHandler(xml.sax.ContentHandler):
 
     def __init__(self):
@@ -214,12 +491,7 @@ class docHandler(xml.sax.ContentHandler):
             self.tag = ""
             self.title = ""
             self.text = ""
-            # print("title: ", title)
-            # print("body: ", body)
-            # print("info: ", info)
-            # print("categories: ", cat)
-            # print("links: ", links)
-            # print("references: ", ref)
+  
 
 
 def Parser(filename):
@@ -232,9 +504,17 @@ def Parser(filename):
 
 if __name__ == "__main__":
     Parser(sys.argv[1])
-    inverted_index = {"inverted_index": inverted_index}
-    folder = sys.argv[2]
-    with open(folder + "/inverted_index.txt", "w+") as file:
-        file.write(json.dumps(inverted_index))
-    with open(folder + "/title.txt", "w+") as file:
-        file.write(json.dumps(title_dic))
+    # inverted_index = {"inverted_index": inverted_index}
+    # folder = sys.argv[2]
+    offset = writeIntoFile(inverted_index, title_dic, fileCount, offset)
+    with open('../data/fileNumbers.txt', 'w') as f:
+        f.write(str(currPage))
+
+    indexMap = defaultdict(list)
+    title_dic = {}
+    fileCount += 1
+    mergeFiles(fileCount)
+    # with open(folder + "/inverted_index.txt", "w+") as file:
+    #     file.write(json.dumps(inverted_index))
+    # with open(folder + "/title.txt", "w+") as file:
+    #     file.write(json.dumps(title_dic))
--- Mini Project/search.py
+++ Mini Project/search.py
@@ -1,111 +1,188 @@
-import re
-import json
-import sys
-import os
-from index import stemming, tokenization, removeStopwords
 from collections import defaultdict
-from collections import Counter
+from index import stemming, tokenization, removeStopwords
+import pdb
+import sys
+import threading
+import math
+import re
+import timeit
+
+offset = []
+titleOffset = []
+
+def findFileNo(low, high, offset, word, f, typ='str'):
+
+    while low < high:
+        mid = int((low + high) / 2)
+        f.seek(offset[mid])
+        wordPtr = f.readline().strip().split()
+        if typ == 'int':
+            # print(word, wordPtr[0])
+            if int(word) == int(wordPtr[0]):
+                return wordPtr[1:], mid
+            elif int(word) > int(wordPtr[0]):
+                low = mid + 1
+            else:
+                high = mid
+        else:
+            if word == wordPtr[0]:
+                return wordPtr[1:], mid
+            elif word > wordPtr[0]:
+                low = mid + 1
+            else:
+                high = mid
+    return [], -1
 
 
-inverted_index = defaultdict(list)
-title = {}
-def read_file(testfile):
+def findDocs(filename, fileNo, field, word, fieldFile):
 
-    with open(testfile, 'r') as file:
-        queries = file.readlines()
-    return queries
-
-
-def write_file(outputs, path_to_output):
-
-    with open(path_to_output, 'w+') as file:
-        # print(outputs)
-        for output in outputs:
-            for line in output:
-                file.write(line.strip() + '\n')
-            file.write('\n')
+    fieldOffset = []
+    docFreq = []
+    with open('../data/offset_' + field + fileNo + '.txt') as f:
+        for line in f:
+            offset, df = line.strip().split()
+            fieldOffset.append(int(offset))
+            docFreq.append(int(df))
+    docList, mid = findFileNo(0, len(fieldOffset), fieldOffset, word, fieldFile)
+    return docList, docFreq[mid]
 
 
-def search(path_to_index, queries):
+def fieldQuery(words, fields, fvocab):
+
+    docList = defaultdict(dict)
+    docFreq = {}
+    for i in range(len(words)):
+        word = words[i]
+        field = fields[i]
+        docs, mid = findFileNo(0, len(offset), offset, word, fvocab)
+        #print('docs')
+        #print(docs)
+        if len(docs) > 0:
+            fileNo = docs[0]
+            filename = '../data/' + field + str(fileNo) + '.txt'
+            fieldFile = open(filename, 'r')
+            returnedList, df = findDocs(filename, fileNo, field, word, fieldFile)
+            docList[word][field] = returnedList
+            docFreq[word] = df
+    return docList, docFreq
+
+
+def simpleQuery(words, fvocab):
+
+    docList = defaultdict(dict)
+    docFreq = {}
+    fields = ['t', 'b', 'i', 'c', 'r', 'l']
+    for word in words:
+        docs, mid = findFileNo(0, len(offset), offset, word, fvocab)
+        if len(docs) > 0:
+            fileNo = docs[0]
+            docFreq[word] = docs[1]
+            for field in fields:
+                #print(word, field)
+                filename = '../data/' + field + str(fileNo) + '.txt'
+                fieldFile = open(filename, 'r')
+                returnedList, _ = findDocs(filename, fileNo, field, word, fieldFile)
+                docList[word][field] = returnedList
+    return docList, docFreq
+
+
+def rank(results, docFreq, nfiles, qtype):
+
+    docs = defaultdict(float)
+
+    #s1 = defaultdict(int)
+    #s2 = defaultdict(int)
+    queryIdf = {}
+
+    for key in docFreq:
+        queryIdf[key] = math.log((float(nfiles) - float(docFreq[key]) + 0.5) / ( float(docFreq[key]) + 0.5))
+        docFreq[key] = math.log(float(nfiles) / float(docFreq[key]))
+
+    for word in results:
+        fieldWisePostingList = results[word]
+        for field in fieldWisePostingList:
+            if len(field) > 0:
+                field = field
+                postingList = fieldWisePostingList[field]
+                if field == 't':
+                    factor = 0.25
+                if field == 'b':
+                    factor = 0.25
+                if field == 'i':
+                    factor = 0.20
+                if field == 'c':
+                    factor = 0.1
+                if field == 'r':
+                    factor = 0.05
+                if field == 'l':
+                    factor = 0.05
+                for i in range(0, len(postingList), 2):
+                    #s1[postingList[i]] += float((1 + math.log(float(postingList[i+1]))) * docFreq[word]) ** 2
+                    #s2[postingList[i]] += float(queryIdf[word]) ** 2
+                    docs[postingList[i]] += float( factor * (1+math.log(float(postingList[i+1]))) * docFreq[word])
     
-    output = []
-    for query in queries:
-        # print(query)
-        answer = []
+    #for key in docs:
+    #    docs[key] /= float(math.sqrt(s1[key])) * float(math.sqrt(s2[key]))
+
+    return docs
+
+
+def search():
+
+    print('Loading Search Engine\n')
+    with open('../data/titleOffset.txt', 'r') as f:
+        for line in f:
+            titleOffset.append(int(line.strip()))
+
+    with open('../data/offset.txt', 'r') as f:
+        for line in f:
+            offset.append(int(line.strip()))
+    
+    fvocab = open('../data/vocab.txt', 'r')
+    titleFile = open('../data/title.txt', 'r')
+    f = open('../data/fileNumbers.txt', 'r')
+    nfiles = int(f.read().strip())
+    f.close()
+
+    # d = Doc()
+
+    while True:
+        query = input('\nType in your query:\n')
+        start = timeit.default_timer()
         query = query.lower()
-        if re.match(r'title|body|infobox|category|ref|link:', query):
-            query = query.split(" ")
-            doc_list = []
-            for token in query:
-                token = token.split(":")
-                stemmed_token = stemming(token)
-                word = stemmed_token[1]
-                # print(word)
-                # print(token[0])
-                if "body" == token[0]:
-                    for x in inverted_index[word]:
-                        if 'b' in x:
-                            doc_list.append(re.split('b|t|r|c|l|i', x)[0])
-                if "title" == token[0]:
-                    for x in inverted_index[word]:
-                        if 't' in x:
-                            doc_list.append(re.split('b|t|r|c|l|i', x)[0])
-                if "infobox" == token[0]:
-                    for x in inverted_index[word]:
-                        if 'i' in x:
-                            doc_list.append(re.split('b|t|r|c|l|i', x)[0])
-                if "category" == token[0]:
-                    for x in inverted_index[word]:
-                        if 'c' in x:
-                            doc_list.append(re.split('b|t|r|c|l|i', x)[0])
-                if "ref" == token[0]:
-                    for x in inverted_index[word]:
-                        if 'r' in x:
-                            doc_list.append(re.split('b|t|r|c|l|i', x)[0])
-                if "link" == token[0]:
-                    for x in inverted_index[word]:
-                        if 'l' in x:
-                            doc_list.append(re.split('b|t|r|c|l|i', x)[0])
-            # print(doc_list)
+    
+        if re.match(r'[t|b|i|c|r|l]:', query):
+            words = re.findall(r'[t|b|c|i|l|r]:([^:]*)(?!\S)', query)
+            tempFields = re.findall(r'([t|b|c|i|l|r]):', query)
+            tokens = []
+            fields = []
+            for i in range(len(words)):
+                for word in words[i].split():
+                    fields.append(tempFields[i])
+                    tokens.append(word)
+            tokens = removeStopwords(tokens)
+            tokens = stemming(tokens)
+            results, docFreq = fieldQuery(tokens, fields, fvocab)
+            results = rank(results, docFreq, nfiles, 'f')
         else:
             tokens = tokenization(query)
             tokens = removeStopwords(tokens)
             tokens = stemming(tokens)
-            doc_list = []
-            for token in tokens:
-                # print(token)
-                for x in inverted_index[token]:
-                    doc_list.append(re.split('b|t|r|c|l|i', x)[0])
-        result = Counter(doc_list)
-        sorted_result = sorted(result.items(), key = lambda kv:kv[1], reverse = True)
-        # print(sorted_result)
-        i = 0;
-        for key, value in sorted_result:
-            # print(title[key])
-            answer.append(title[key])
-            i += 1
-            if i == 10:
-                break
-        output.append(answer)
-    return output
+            results, docFreq = simpleQuery(tokens, fvocab)
+            results = rank(results, docFreq, nfiles, 's')
 
 
-def main():
-    global inverted_index
-    global title
-    path_to_index = sys.argv[1]
-    testfile = sys.argv[2]
-    path_to_output = sys.argv[3]
-    with open(os.path.join(path_to_index, 'inverted_index.txt')) as f:
-        inverted_index = defaultdict(list, json.load(f)['inverted_index'])
-
-    with open(os.path.join(path_to_index, 'title.txt')) as t:
-        title = json.load(t)
-
-    queries = read_file(testfile)
-    outputs = search(path_to_index, queries)
-    write_file(outputs, path_to_output)
+        print('\nResults:\n')
+        if len(results) > 0:
+            results = sorted(results, key=results.get, reverse=True)
+            results = results[:10]
+            for key in results:
+                title, _ = findFileNo(0, len(titleOffset), titleOffset, key, titleFile, 'int')
+                print(' '.join(title))
+        end = timeit.default_timer()
+        print('Time taken =', end-start)
 
 
 if __name__ == '__main__':
-    main()
+
+    search()
